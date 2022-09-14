014822d955d44c4a8f168a0d2568911bb895463f
LEMON シ <87497636+LemonProg@users.noreply.github.com>
2022-09-14 12:31:01 +0200

    Add files via upload

---
+++ Base/main.py
@@ -0,0 +1,54 @@
+﻿import time
+import keyboard
+from pystyle import Colors, Colorate
+
+print(Colorate.Horizontal(Colors.rainbow, """
+      
+ █████╗ ███████╗██╗  ██╗    ███████╗ ██████╗██████╗ ██╗██████╗ ████████╗
+██╔══██╗██╔════╝██║ ██╔╝    ██╔════╝██╔════╝██╔══██╗██║██╔══██╗╚══██╔══╝
+███████║█████╗  █████╔╝     ███████╗██║     ██████╔╝██║██████╔╝   ██║   
+██╔══██║██╔══╝  ██╔═██╗     ╚════██║██║     ██╔══██╗██║██╔═══╝    ██║   
+██║  ██║██║     ██║  ██╗    ███████║╚██████╗██║  ██║██║██║        ██║   
+╚═╝  ╚═╝╚═╝     ╚═╝  ╚═╝    ╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝╚═╝        ╚═╝                                                                                                                                                             
+      """))
+
+print("Press F2 to start the scipt !")
+print("Hold F3 a few seconds to stop the script !")
+
+azerty = "z"
+qwerty = "w"
+
+def afk():
+    loop = True
+    while loop:
+        if keyboard.is_pressed("F3"):
+            loop = False
+        else:
+            time.sleep(2)
+            keyboard.press_and_release("escape")
+            time.sleep(0.5)
+            keyboard.press_and_release("escape")
+            time.sleep(0.5)
+            keyboard.press(azerty)
+            time.sleep(1)
+            keyboard.release(azerty)
+            keyboard.press(qwerty)
+            time.sleep(1)
+            keyboard.release(qwerty)
+            time.sleep(0.5)
+            keyboard.press("s")
+            time.sleep(1)
+            keyboard.release("s")
+
+start = True
+while start:
+    if keyboard.is_pressed("F2"):
+        print("\nScript successfully start :)")
+        start = False 
+        afk()
+
+        
+        
+
+
+            
---
+++ Base/required library.py
@@ -0,0 +1,4 @@
+import os 
+
+os.system("pip3 install keyboard")
+os.system("pip3 install pystyle")
